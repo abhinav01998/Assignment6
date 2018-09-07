b59f85177f94e7d118c83a07b9c3111fd4c54b9f
abhinav01998 <41861306+abhinav01998@users.noreply.github.com>
2018-09-07 19:18:19 +0530

    Initial commit

---
+++ NotExtendable.java
@@ -0,0 +1,11 @@
+ final class demo{
+void display(){
+System.out.println("This class cannot be extended further");
+}
+}
+class NotExtendable {
+public static void main(String args[]){
+xyz obj=new xyz();
+obj.display();
+}
+}
